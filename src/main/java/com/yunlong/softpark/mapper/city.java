782dd5a0d400fe30addffed009c6b4d228989f61
Firstname Lastname <1445748491@qq.com>
2020-07-19 21:21:25 +0800

    项目框架

---
+++ src/main/java/com/yunlong/softpark/mapper/city.java
@@ -0,0 +1,17 @@
+package com.yunlong.softpark.mapper;
+
+import lombok.Data;
+
+/**
+ * @Author: Cui
+ * @Date: 2020/7/18
+ * @Description:
+ */
+@Data
+public class city {
+
+    private String name;
+    private int num;
+    private int gender;
+
+}
